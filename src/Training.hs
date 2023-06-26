6a56a63e911cfc52c8d1fd29b1a3ed435370e20c
Steven <steven.a.pinkerton@gmail.com.com>
2023-06-26 22:09:51 +0100

    Refactored

--- src/Training.hs
+++ src/Training.hs
@@ -9,6 +9,7 @@ import Utility (oneHotEncode, shuffle, updateNetworkBatch)
 import System.Random ()
 import Data.List ( zipWith3 )
 import Data.Maybe ( fromJust )
+import Data.List.Split (chunksOf)
 
 -- | Train the network over multiple epochs.
 train :: RandomGen g => Network -> [(Inputs, Target)] -> LearningRate -> Int -> g -> (Network, g)
@@ -19,30 +20,32 @@ train' network trainingData learningRate epochs =
   foldl' (trainEpoch trainingData learningRate) (return network) [1 .. epochs]
 
 -- | Train the network for one epoch.
+-- | Train the network for one epoch.
 trainEpoch :: RandomGen g => [(Inputs, Target)] -> LearningRate -> Rand g Network -> Int -> Rand g Network
 trainEpoch trainingData learningRate networkM _ = do
   network <- networkM
   shuffledData <- shuffle trainingData
-  return $ foldl' (trainMiniBatch learningRate) network (miniBatches shuffledData)
+  let miniBatchedData = miniBatches 10 shuffledData -- The batch size is set to 10, but you can adjust this
+  return $ concatMap (trainMiniBatch learningRate network) miniBatchedData
 
--- | Split the training data into mini-batches.
-miniBatches :: [(Inputs, Target)] -> [[(Inputs, Target)]]
-miniBatches = undefined -- Implement this function
+-- | Split the training data into mini-batches of a given size.
+miniBatches :: Int -> [(Inputs, Target)] -> [[(Inputs, Target)]]
+miniBatches = chunksOf
 
 -- | Train the network on a mini-batch of training examples.
 trainMiniBatch :: LearningRate -> Network -> [(Inputs, Target)] -> Network
 trainMiniBatch learningRate network miniBatch =
-  let (inputs, targets) = unzip miniBatch
+  let (inputs, targets) = Prelude.unzip miniBatch
       maybeOutputs = mapM (calculateNetworkOutputs network) inputs
    in case maybeOutputs of
         Just outputs ->
           let targets' = map oneHotEncode targets
               flattenedOutputs = map (fromJust . viaNonEmpty last) outputs
-              _losses = zipWith crossEntropyLoss targets' flattenedOutputs
-              maybeDeltas = sequence $ zipWith3 calculateNetworkErrorDeltas (repeat network) targets' outputs -- use `outputs` here
+              _losses = Prelude.zipWith crossEntropyLoss targets' flattenedOutputs
+              maybeDeltas = sequence $ Data.List.zipWith3 calculateNetworkErrorDeltas (repeat network) targets' outputs -- use `outputs` here
            in case maybeDeltas of
                 Just deltas ->
-                  let inputsAndDeltas = zip inputs deltas
+                  let inputsAndDeltas = Prelude.zip inputs deltas
                    in updateNetworkBatch learningRate network inputsAndDeltas
                 Nothing -> network
         Nothing -> network
